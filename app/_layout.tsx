a1df9d21c3b3eb0adb2095ec8775e395bbabc942
Rozer Shrestha <rozer.shrestha611@gmail.com>
2025-12-01 13:35:04 +0545

    latest

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
-
+import './globals.css';
+import {SCREEN_ORIENTATION_ATTRIBUTE} from "@expo/config-plugins/build/android/Orientation";
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+      <Stack.Screen
+          name="(tabs)"
+          options={{headerShown: false}}
+      />
+      <Stack.Screen
+          name="movie/[id"
+          options={{headerShown: false}}
+      />
+  </Stack>
 }
